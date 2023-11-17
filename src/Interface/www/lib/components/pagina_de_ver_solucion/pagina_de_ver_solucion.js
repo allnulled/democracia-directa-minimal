10320b5c8bb7e7178dddd8b7ad39b10f2a70d97f
allnulled <todosanulados@gmail.com>
2023-11-17 10:56:04 +0100

    actualizado para votos

--- src/Interface/www/lib/components/pagina_de_ver_solucion/pagina_de_ver_solucion.js
+++ src/Interface/www/lib/components/pagina_de_ver_solucion/pagina_de_ver_solucion.js
@@ -14,13 +14,13 @@ window.PaginaDeVerSolucion = Castelog.metodos.un_componente_vue2("PaginaDeVerSol
  + "      </div>"
  + "      <div class=\"padding_1 padding_top_0 row\">"
  + "        <div class=\"col-4\">"
- + "          <button class=\"width_100 danger_button\" v-on:click=\"() => root.votarEnContra('id_solucion', $route.params.id_solucion)\">Votar en contra</button>"
+ + "          <button class=\"width_100 danger_button\" :disabled=\"haVotadoYa\" v-on:click=\"() => votarEnContra('id_problema', this.$route.params.id_problema)\">Votar en contra</button>"
  + "        </div>"
  + "        <div class=\"col-4\">"
- + "          <button class=\"width_100 neutral_button\" v-on:click=\"() => root.retirarVoto('id_solucion', $route.params.id_solucion)\">Retirar voto</button>"
+ + "          <button class=\"width_100 neutral_button\" :disabled=\"!haVotadoYa\" v-on:click=\"() => retirarVoto(votoPrevio.id)\">Retirar voto</button>"
  + "        </div>"
  + "        <div class=\"col-4\">"
- + "          <button class=\"width_100 success_button\" v-on:click=\"() => root.votarAFavor('id_solucion', $route.params.id_solucion)\">Votar a favor</button>"
+ + "          <button class=\"width_100 success_button\" :disabled=\"haVotadoYa\" v-on:click=\"() => votarAFavor('id_problema', this.$route.params.id_problema)\">Votar a favor</button>"
  + "        </div>"
  + "      </div>"
  + "    </WinVentana>"
@@ -30,7 +30,9 @@ required:true
 }
 },
 data() {try {
-return { busqueda:{ filtros:JSON.stringify([ [ "id_solucion",
+return { votoPrevio:undefined,
+haVotadoYa:false,
+busqueda:{ filtros:JSON.stringify([ [ "id_solucion",
 "=",
 this.$route.params.id_solucion ] ], null, 2)
 }
@@ -41,7 +43,65 @@ throw error;
 }
 
 },
-methods:{ 
+methods:{ async votarEnContra( propiedad,
+valor ) {try {
+(await this.root.votarEnContra( propiedad,
+valor ));
+(await this.obtenerVoto(  ));
+this.$forceUpdate( true );
+} catch(error) {
+console.log(error);
+throw error;
+}
+
+},
+async retirarVoto( propiedad,
+valor ) {try {
+(await this.root.retirarVoto( propiedad,
+valor ));
+(await this.obtenerVoto(  ));
+this.$forceUpdate( true );
+} catch(error) {
+console.log(error);
+throw error;
+}
+
+},
+async votarAFavor( propiedad,
+valor ) {try {
+(await this.root.votarAFavor( propiedad,
+valor ));
+(await this.obtenerVoto(  ));
+this.$forceUpdate( true );
+} catch(error) {
+console.log(error);
+throw error;
+}
+
+},
+async obtenerVoto() {try {
+const respuesta_1 = (await Castelog.metodos.una_peticion_http("/ConsultarDatos", "POST", { tabla:"Voto",
+filtros:[ [ "id_solucion",
+"=",
+parseInt( this.$route.params.id_solucion ) ] ]
+}, { authorization:this.root.tokenDeSesion
+}, null, error => {
+this.root.gestionarError( error );}));
+if(!(respuesta_1.data.data.mensaje === "La consulta fue realizada correctamente")) throw new Error("Error en fichero [-] en posición [3260-3367=75:59-76:107] cuando: " + "compruebo que respuesta_1.data.data.mensaje es igual que \"La consulta fue realizada correctamente\"");
+if(respuesta_1.data.data.datos.length > 0) {
+this.haVotadoYa = true;
+this.votoPrevio = respuesta_1.data.data.datos[ 0 ];
+}
+else {
+this.haVotadoYa = false;
+this.votoPrevio = undefined;
+}
+} catch(error) {
+console.log(error);
+throw error;
+}
+
+}
 },
 watch:{ 
 },
